3cab9f564a886383fe453612717141242e23b1f0
Chris Carey <chriscarey703@gmail.com>
2021-07-19 11:21:53 -0400

    Render mapbox zoomed into NYC.

---
+++ src/scripts/app.js
@@ -0,0 +1,22 @@
+import * as constants from './constants.js';
+import {tokens} from '../../private/tokens.js';
+
+function init() {
+  mapboxgl.accessToken = tokens.mapbox;
+}
+
+function drawMap() {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: constants.MAP_INIT_STYLE,
+    center: [constants.MAP_INIT_LON, constants.MAP_INIT_LAT],
+    zoom: constants.MAP_INIT_ZOOM,
+  });
+}
+
+function run() {
+  init();
+  drawMap();
+}
+
+document.addEventListener('DOMContentLoaded', run);
