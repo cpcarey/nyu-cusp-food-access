e2d461fa7293ffd6fc14651e9e822a9e085c1d2a
Chris Carey <chriscarey703@gmail.com>
2021-08-01 14:10:59 -0400

    Encapsulate network analysis visualization in separate class.

--- src/DataMap.jsx
+++ src/DataMap.jsx
@@ -1,10 +1,10 @@
 import * as constants from './constants.js';
 import React, {useRef, useEffect, useState} from 'react';
-import cbgData from './data/nyc_cbg_centroids.json';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import {tokens} from 'private-tokens';
 import csvParse from 'csv-parse/lib/sync';
 import csvDataUrl from './data/20210301_supermarkets.csv';
+import {NetworkAnalysis} from './analysis/NetworkAnalysis.js';
 
 mapboxgl.accessToken = tokens.mapbox;
 
@@ -31,52 +31,7 @@ export function DataMap() {
           }
         });
 
-      const cbgCoordMap =
-          new Map(cbgData.features.map((feature) => {
-            return [
-              feature.properties['CensusBlockGroup'],
-              feature.geometry.coordinates,
-            ];
-          }));
-
-      const cbgVisitCoordsMap = new Map();
-      for (const [cbgId, visitsByCbg] of csvMap.entries()) {
-        cbgVisitCoordsMap.set(
-            cbgId,
-            visitsByCbg.map((visits, index) => {
-              // TODO: Filter out self-visits?
-              return {
-                coord: cbgCoordMap.get(cbgIds[index]),
-                value: visits,
-              };
-            })
-            // XXX: cbgCoordMap should never return undefined.
-            .filter(({coord}) => coord)
-            .filter(({value}) => value));
-      }
-
-      const cbgVisitFeaturesMap = new Map();
-      for (const [cbgId, visitCoords] of cbgVisitCoordsMap.entries()) {
-        cbgVisitFeaturesMap.set(
-            cbgId,
-            visitCoords.map(({coord, value}) => {
-              const ratio = Math.min(value / 30, 1);
-              return {
-                type: 'Feature',
-                geometry: {
-                  type: 'LineString',
-                  coordinates: [
-                    cbgCoordMap.get(cbgId),
-                    coord,
-                  ],
-                },
-                properties: {
-                  'opacity': ratio * 0.8,
-                  'width': ratio * 5,
-                },
-              };
-            }));
-      }
+      const networkAnalysis = new NetworkAnalysis(cbgIds, csvMap);
 
       const map = new mapboxgl.Map({
         container: mapContainerRef.current,
@@ -86,32 +41,7 @@ export function DataMap() {
       });
 
       map.on('load', () => {
-        map.addSource('cbgs', {
-          data: cbgData,
-          type: 'geojson',
-        });
-
-        map.addSource('cbgNetwork', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [...cbgVisitFeaturesMap.values()].reduce((a, b) => [...a, ...b]),
-          },
-        });
-
-        map.addLayer({
-          id: 'cbgNetwork',
-          type: 'line',
-          source: 'cbgNetwork',
-          layout: {
-            visibility: 'visible',
-          },
-          paint: {
-            'line-color': '#fff',
-            'line-opacity': ['get', 'opacity'],
-            'line-width': ['get', 'width'],
-          },
-        });
+        networkAnalysis.applyToMap(map);
       });
     })();
   }, [lat, lon, zoom]);
