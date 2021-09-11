2d677e3adc302bd015a5e373619de48d7ec9d655
Chris Carey <chriscarey703@gmail.com>
2021-09-11 16:36:46 -0400

    Add CBG hover response and link to chart panel.

---
+++ client/src/QueryPanel.jsx
@@ -0,0 +1,156 @@
+import {useState} from 'react';
+
+import './Panel.css';
+
+export function QueryPanel({queryState, setQueryState}) {
+  const [expanded, setExpanded] = useState(false);
+
+  const classNamePanel = [
+    'panel',
+    'panel-left',
+    expanded ? 'expanded' : '',
+  ].filter((className) => className.length).join(' ');
+  const classNameIcon = 'fas fa-cog';
+
+  function handleAggregationTypeChange(e, queryState) {
+    setQueryState({
+      ...queryState,
+      aggregationType: parseInt(e.target.value),
+    });
+  }
+
+  function handleAttributeClassChange(e, queryState) {
+    setQueryState({
+      ...queryState,
+      attributeClass: parseInt(e.target.value),
+    });
+  }
+
+  function handleDateEndChange(e, queryState) {
+    // Ensure start date is at least seven days before end date.
+    const dateEndString = e.target.value;
+    const dateEnd = new Date(dateEndString);
+    const dateStartMax = new Date(dateEndString);
+    dateStartMax.setDate(dateEnd.getDate() - 7);
+    let dateStart = new Date(queryState.dateStart);
+    if (dateStart > dateStartMax) {
+      dateStart = dateStartMax;
+    }
+    const dateStartString = dateStart.toISOString().split('T')[0];
+
+    setQueryState({
+      ...queryState,
+      dateEnd: dateEndString,
+      dateStart: dateStartString,
+    });
+  }
+
+  function handleDateStartChange(e, queryState) {
+    // Ensure end date is at least seven days after end date.
+    const dateStartString = e.target.value;
+    const dateStart = new Date(dateStartString);
+    const dateEndMin = new Date(dateStartString);
+    dateEndMin.setDate(dateStart.getDate() + 7);
+    let dateEnd = new Date(queryState.dateEnd);
+    if (dateEnd < dateEndMin) {
+      dateEnd = dateEndMin;
+    }
+    const dateEndString = dateEnd.toISOString().split('T')[0];
+
+    setQueryState({
+      ...queryState,
+      dateEnd: dateEndString,
+      dateStart: dateStartString,
+    });
+  }
+
+  function handleMetricTypeChange(e, queryState) {
+    setQueryState({
+      ...queryState,
+      metricType: parseInt(e.target.value),
+    });
+  }
+
+  return (
+    <div className={classNamePanel}>
+      <div className="panel-controls">
+        <div className="panel-control dropdown">
+          <div>Visualization</div>
+          <div className="select-container">
+            <select>
+              <option value="0">Choropleth</option>
+              <option value="1">Trip Network</option>
+            </select>
+          </div>
+        </div>
+        <div className="panel-control date-picker">
+          <div>Start Date</div>
+          <div>
+            <input
+              max="2021-03-01"
+              min="2020-03-01"
+              onChange={(e) => handleDateStartChange(e, queryState)}
+              type="date"
+              value={queryState.dateStart}
+              />
+          </div>
+        </div>
+        <div className="panel-control date-picker">
+          <div>End Date</div>
+          <div>
+            <input
+              max="2021-03-01"
+              min="2020-03-02"
+              onChange={(e) => handleDateEndChange(e, queryState)}
+              type="date"
+              value={queryState.dateEnd}
+              />
+          </div>
+        </div>
+        <div className="panel-control dropdown">
+          <div>Attribute</div>
+          <div className="select-container">
+            <select>
+              <option value="0">NAICS Code Group</option>
+            </select>
+          </div>
+        </div>
+        <div className="panel-control dropdown">
+          <div>Attribute Class</div>
+          <div className="select-container">
+            <select onChange={(e) => handleAttributeClassChange(e, queryState)}>
+              <option value="0">Supermarkets</option>
+              <option value="1">General Stores</option>
+              <option value="2">Restaurants</option>
+              <option value="3">Community Food Services</option>
+              <option value="4">Supplement Stores</option>
+              <option value="5">Tobacco & Liquor Stores</option>
+            </select>
+          </div>
+        </div>
+        <div className="panel-control dropdown">
+          <div>Metric</div>
+          <div className="select-container">
+            <select onChange={(e) => handleMetricTypeChange(e, queryState)}>
+              <option value="0">Visitor count</option>
+              <option value="1">Visitor density</option>
+            </select>
+          </div>
+        </div>
+        <div className="panel-control dropdown">
+          <div>Aggregation</div>
+          <div className="select-container">
+            <select onChange={(e) => handleAggregationTypeChange(e, queryState)}>
+              <option value="0">Average</option>
+              <option value="1">Median</option>
+              <option value="2">Sum</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <button onClick={() => setExpanded(!expanded)}>
+        <i className={classNameIcon}></i>
+      </button>
+    </div>
+  );
+}
--- client/src/analysis/VisitChoroplethAnalysis.js
+++ client/src/analysis/VisitChoroplethAnalysis.js
@@ -2,20 +2,53 @@ import cbgData from 'data/nyc_cbgs.json';
 import {Analysis} from 'analysis/Analysis';
 import * as util from 'analysis/util';
 
+const DEFAULT_COLOR_MAX = '#ff2200';
+const DEFAULT_COLOR_MIN = '#00adff';
+const DEFAULT_ID = 'cbg1';
+
 export class VisitChoroplethAnalysis extends Analysis {
   /**
-   * @param {string} color Choropleth color
+   * @param {!Map<string, number>} cbgValueMap
+   * @param {function({id: number, value: number}):void} setHoveredCbg
+   * @param {!string} colorMax
+   * @param {!string} colorMin
    * @param {string} id Analysis ID to use within Mapbox
    * @override
    */
-  constructor(cbgValueMap, threshold=100, colorMax='#ff2200', colorMin='#00adff', id='cbg1') {
+  constructor(
+      cbgValueMap, setHoveredCbg, colorMax=DEFAULT_COLOR_MAX,
+      colorMin=DEFAULT_COLOR_MIN, id=DEFAULT_ID) {
     super(cbgValueMap);
 
-    this.colorMin = colorMin;
+    /** @type {string} */
     this.colorMax = colorMax;
 
-    this.threshold = threshold;
+    /** @type {string} */
+    this.colorMin = colorMin;
+
+    /** @type {string} */
     this.id = id;
+
+    /** @type {string} */
+    this.cbgFillLayerId = `${this.id}-fill`;
+
+    /** @type {!Map<string, !GeoJSON.Feature>} */
+    this.cbgIdToFeatureMap = new Map();
+
+    /** @type {!Map<string, [number, number]>} */
+    this.cbgIdToPolygonsMap = new Map();
+
+    /** @type {string} */
+    this.cbgLineLayerId = `${this.id}-line`;
+
+    /** @type {!Map<string, number>} */
+    this.cbgNormalizedValueMap = new Map();
+
+    /** @type {?string} */
+    this.hoveredCbgId = null;
+
+    /** @type {function({id: number, value: number}):void} */
+    this.setHoveredCbg = setHoveredCbg;
 
     this.setCbgValueMap(cbgValueMap);
   }
@@ -29,16 +62,34 @@ export class VisitChoroplethAnalysis extends Analysis {
       return;
     }
 
-    map.addSource(this.id, {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [...this.cbgToFeatureMap.values()],
-      },
+    let source = map.getSource(this.id);
+    if (!source) {
+      map.addSource(this.id, {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [],
+        },
+      });
+      source = map.getSource(this.id);
+    }
+
+    source.setData({
+      type: 'FeatureCollection',
+      features: [...this.cbgIdToFeatureMap.values()],
     });
 
+    function resetLayer(id) {
+      if (map.getLayer(id)) {
+        map.removeLayer(id);
+      }
+    }
+
+    resetLayer(this.cbgFillLayerId);
+    resetLayer(this.cbgLineLayerId);
+
     map.addLayer({
-      id: this.id,
+      id: this.cbgFillLayerId,
       type: 'fill',
       source: this.id,
       layout: {
@@ -51,46 +102,107 @@ export class VisitChoroplethAnalysis extends Analysis {
     },
     util.getFirstSymbolMapLayerId(map));
 
+    map.addLayer({
+      id: this.cbgLineLayerId,
+      type: 'line',
+      source: this.id,
+      layout: {
+        visibility: 'visible',
+      },
+      paint: {
+        'line-color': ['get', 'color'],
+        'line-opacity': [
+          'case',
+          ['boolean', ['feature-state', 'hover'], false],
+          1,
+          0,
+        ],
+        'line-width': ['get', 'lineWidth'],
+      },
+    },
+    this.cbgFillLayerId);
+
+    const setHoveredState = (id, hover) => {
+      if (id) {
+        map.setFeatureState({id, source: this.id}, {hover});
+        if (hover && this.cbgValueMap.has(id)) {
+          this.setHoveredCbg({id, value: this.cbgValueMap.get(id)});
+        }
+      }
+      map.getCanvas().style.cursor = (hover && id && this.getRatio_(id)) ? 'pointer' : 'grab';
+    }
+
+    map.on('mousemove', this.cbgFillLayerId, (e) => {
+      if (e.features.length) {
+        setHoveredState(this.hoveredCbgId, false);
+        this.hoveredCbgId = e.features[0].id;
+        setHoveredState(this.hoveredCbgId, true);
+      } else {
+        map.getCanvas().style.cursor = 'grab';
+      }
+    });
+
+    map.on('mouseleave', this.cbgFillLayerId, (e) => {
+      setHoveredState(this.hoveredCbgId, false);
+      this.hoveredCbgId = null;
+    });
+
     super.applyToMap(map);
   }
 
+  /** @param {!Map<string, number> cbgValueMap */
   setCbgValueMap(cbgValueMap) {
     this.cbgValueMap = cbgValueMap;
     this.cbgNormalizedValueMap = util.normalizeSigmaMap(cbgValueMap, 4.0);
-    this.cbgToPolygonsMap = util.getCbgToCoordsMap(cbgData);
-    this.cbgToFeatureMap = this.getCbgToFeatureMap_();
+    this.cbgIdToPolygonsMap = util.getCbgIdToCoordsMap(cbgData);
+    this.cbgIdToFeatureMap = this.getCbgIdToFeatureMap_();
     if (this.map) {
       this.map.getSource(this.id).setData({
         type: 'FeatureCollection',
-        features: [...this.cbgToFeatureMap.values()],
+        features: [...this.cbgIdToFeatureMap.values()],
       });
     }
   }
 
-  convertCoordVisitToMultiPolygonFeature_(cbgId) {
-    const ratio =
-        this.cbgNormalizedValueMap.has(cbgId)
+  getRatio_(cbgId) {
+    return this.cbgNormalizedValueMap.has(cbgId)
             ? (this.cbgNormalizedValueMap.get(cbgId) - 0.5) * 2
             : 0;
+  }
+
+  /**
+   * @param {string} cbgId
+   * @return {!GeoJson.Feature}
+   * @private
+   */
+  convertCoordVisitToMultiPolygonFeature_(cbgId) {
+    const ratio = this.getRatio_(cbgId);
     return {
+      id: cbgId,
       type: 'Feature',
       geometry: {
         type: 'MultiPolygon',
-        coordinates: this.cbgToPolygonsMap.get(cbgId),
+        coordinates: this.cbgIdToPolygonsMap.get(cbgId),
       },
       properties: {
         'color': ratio > 0 ? this.colorMax : this.colorMin,
-        'opacity': Math.abs(ratio),
+        'lineWidth': ratio ? 2 : 0,
+        'opacity': Math.abs(ratio) * 0.75,
       },
     };
   }
 
-  getCbgToFeatureMap_() {
-    const cbgToFeatureMap = new Map();
-    for (const cbgId of this.cbgToPolygonsMap.keys()) {
-      cbgToFeatureMap.set(
+  /**
+   * @param {string} cbgId
+   * @return {!Map<string, !GeoJSON.Feature>}
+   * @private
+   */
+  getCbgIdToFeatureMap_() {
+    const cbgIdToFeatureMap = new Map();
+    for (const cbgId of this.cbgIdToPolygonsMap.keys()) {
+      cbgIdToFeatureMap.set(
           cbgId, this.convertCoordVisitToMultiPolygonFeature_(cbgId));
     }
-    return cbgToFeatureMap;
+    return cbgIdToFeatureMap;
   }
 }
